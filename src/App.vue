e073da49e96e9810b07472116b7e1ff559a83450
dnavault <47720676+dnavault@users.noreply.github.com>
2021-03-10 13:50:34 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <DataGrid 
+      :colHeaders="['ID','Name']"
+      :rowData="[{id:1,name: 'Test'},{id:2,name: 'Test2'}]"
+     />
+  </div>
+</template>
+
+<script>
+import DataGrid from "./components/DataGrid";
+
+export default {
+  name: "App",
+  
+  components: {
+    DataGrid
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataGrid.vue
@@ -0,0 +1,38 @@
+<template>
+ <div>
+    <ul>
+      <li v-for="col in colHeaders" :key="col">{{ col }}</li>
+    </ul>
+    <ul>
+      <li style="display:block" v-for="row in rowData" :key="row.id">{{ row.id }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataGrid",
+  props: {
+    colHeaders: Array,
+    rowData: Array
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
